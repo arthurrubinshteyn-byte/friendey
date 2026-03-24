a8d24eda4274b2b82a603512a34249be4d72b01e
arthurrubinshteyn-byte <arthurrubinshteyn@gmail.com>
2026-03-24 16:13:41 -0400

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client' 
 import { useEffect, useState, useCallback, useRef } from 'react'
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
